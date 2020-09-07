b03aa9de70c53145b621ab3a3cbeb991e25396e7
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-09-06 23:33:13 -0300

    style: remove unnecessary comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-// const app = express()
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
