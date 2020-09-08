e68ebe39540a344b19204e2aa064c320d6ff009d
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-09-07 22:50:43 -0300

    feat: added knex connection factory

---
+++ src/database/knex.js
@@ -0,0 +1,4 @@
+const knexfile = require('../knexFile')
+const knex = require('knex')(knexfile.development)
+
+module.exports = knex
---
+++ src/knexFile.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: '3306',
+      user: process.env.DB_USER,
+      password: process.env.DB_DATABASE,
+      database: process.env.DB_PASSWORD
+    },
+    migrations: {
+      tableName: 'knexMigrations',
+      directory: `${__dirname}/database/migrations`
+    }
+  }
+}
