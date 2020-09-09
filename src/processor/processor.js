d4c04b54db5dad5a316ca1a5c699e339524754cd
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-09-09 01:11:48 -0300

    feat: added insertQueueItem function

--- src/processor/processor.js
+++ src/processor/processor.js
@@ -1,4 +1,6 @@
 
+const log = require('bole')('processor/processor')
+const knex = require('../database/knex')
 const { validator } = require('../validator')
 const { safeArray } = require('../utils')
 const inputSchema = require('../schemes/input.json')
@@ -6,7 +8,31 @@ const inputSchema = require('../schemes/input.json')
 /**
  * @typedef InputValue
  * @property { String } id
- * @property { String } groupId
+ * @property { String } feedbackUrl
+ * @property { String } baseImagePath
+ * @property { InputValuePositions} positions
+ * @property { InputValueOptions} watermarkOptions
+ */
+
+/**
+ * @typedef InputValuePositions
+ * @property { Number } x
+ * @property { Number } y
+ * @property { Number } height
+ * @property { Number } width
+ */
+
+/**
+ * @typedef InputValueOptions
+ * @property { 'text' | 'image' } type
+ * @property { InputValueDetails } details
+ */
+/**
+ * @typedef InputValueDetails
+ * @property { String } [text]
+ * @property { Number } [size]
+ * @property { String } [color]
+ * @property { String } [path]
  */
 
 /**
@@ -17,7 +43,6 @@ const inputSchema = require('../schemes/input.json')
 /**
  * @typedef QueueItem
  * @property { String } id
- * @property { String } groupId
  * @property { 'queued' | 'error' } status
  * @property { Array<QueueItemDetail> } [details]
  */
@@ -25,35 +50,79 @@ const inputSchema = require('../schemes/input.json')
 /**
  * Init the queue to proccess
  * @param { Array<InputValue> } values - The value of request
- * @return { Array<QueueItem> }
+ * @return { Array<Promise<QueueItem>> }
  */
-function initQueue (values) {
-  return safeArray(values)
-    .reduce((data, item) => {
-      const controller = `${item.groupId}-${item.id}`
-
-      if (!data.controller.includes(controller)) {
-        const result = {
-          id: item.id,
-          groupId: item.groupId,
-          status: 'queued'
-        }
-
-        if (!validator.validate(item, inputSchema)) {
-          result.status = 'error'
-          result.details = validator
-            .getErrors()
-            .map(_ => ({ error: _.message }))
-        }
-
-        // TODO: add a real queue implementation
-
-        data.controller.push(controller)
-        data.result.push(result)
+async function initQueue (values) {
+  const promises = safeArray(values)
+    .map(async item => {
+      const result = {
+        id: item.id,
+        status: 'queued'
       }
 
+      if (!validator.validate(item, inputSchema)) {
+        result.status = 'error'
+        result.details = validator
+          .getErrors()
+          .map(_ => ({ error: _.message }))
+      } else {
+        const isQueued = await insertQueueItem(item)
+
+        if (!isQueued) {
+          result.status = 'error'
+          result.details = [{ error: 'error to insert the item into the queue' }]
+        }
+      }
+
+      return result
+    })
+
+  return Promise
+    .all(promises)
+    .then(data => {
+      initProcessor()
       return data
-    }, { result: [], controller: [] }).result
+    })
+}
+
+/**
+ * Insert the item into database queue
+ * @param { InputValue } item - Item of request
+ * @return { Boolean }
+ */
+async function insertQueueItem (item) {
+  try {
+    const date = new Date()
+
+    await knex('queue')
+      .insert({
+        status: 'queued',
+        attempt: 1,
+        created_at: date,
+        updated_at: date,
+        item_id: item.id,
+        feedback_url: item.feedbackUrl,
+        base_image_path: item.baseImagePath,
+        type: item.watermarkOptions.type,
+        details_text: item.watermarkOptions.details.text,
+        details_size: item.watermarkOptions.details.size,
+        details_color: item.watermarkOptions.details.color,
+        details_path: item.watermarkOptions.details.path,
+        position_x: item.positions.x,
+        position_y: item.positions.y,
+        position_height: item.positions.height,
+        position_width: item.positions.width
+      })
+
+    return true
+  } catch (error) {
+    log.error(error.sqlMessage, 'error to insert item into queue')
+    return false
+  }
+}
+
+function initProcessor () {
+  // todo
 }
 
 module.exports = {
