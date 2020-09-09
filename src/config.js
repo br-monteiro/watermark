0203218688bdbae3d6e0e1248944d2c3af3a9bfe
br-monteiro <edson.monteiro@chaordicsystems.com>
2020-09-09 01:09:25 -0300

    refactor: changed configs

--- src/config.js
+++ src/config.js
@@ -1,12 +1,56 @@
+require('dotenv').config()
+
 const config = module.exports
-const PRODUCTION = process.env.NODE_ENV === 'production'
 
 config.express = {
   port: process.env.EXPRESS_PORT || 3000,
   ip: '127.0.0.1'
 }
 
-if (PRODUCTION) {
-  // for example
-  config.express.ip = '0.0.0.0'
+config.knex = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: '3306',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+    },
+    migrations: {
+      tableName: 'knexMigrations',
+      directory: `${__dirname}/database/migrations`
+    }
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: '3306',
+      user: process.env.DB_USER,
+      password: process.env.DB_DATABASE,
+      database: process.env.DB_PASSWORD
+    },
+    migrations: {
+      tableName: 'knexMigrations',
+      directory: `${__dirname}/database/migrations`
+    }
+  }
+}
+
+config.staticPaths = {
+  input: `${__dirname}/../static/input/`,
+  output: `${__dirname}/../static/output/`
+}
+
+config.aws = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  region: process.env.AWS_REGION || 'us-east-1'
+}
+
+config.bucketBase = `${__dirname}/../.bucket/`
+
+config.env = {
+  PRD: process.env.NODE_ENV === 'production'
 }
