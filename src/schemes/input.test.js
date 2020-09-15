60ca36062719a055b6e5c1928f137248524323e1
Edinho <bruno.monteirodg@gmail.com>
2020-09-14 23:34:16 -0300

    test: changed tests for new schema

--- src/schemes/input.test.js
+++ src/schemes/input.test.js
@@ -7,40 +7,26 @@ describe('JSON Schemes - input', () => {
 
   beforeEach(() => {
     data = {
-      id: '199299',
-      feedbackUrl: 'https://teste.com',
-      baseImagePath: 'images/bucket-name/test.png',
-      positions: {
-        x: 370,
-        y: 470,
-        height: 80,
-        width: 150
-      }
+      transactionId: '31313e1ggnkerkn',
+      feedbackUrl: 'https://teste.com/queue',
+      watermarkPath: 'images/bucket-name/stamp.png',
+      images: [
+        {
+          positions: {
+            x: 370,
+            y: 470,
+            height: 80,
+            width: 150
+          },
+          baseImagePath: 'images/bucket-name/background.png',
+          s3ImagePath: 'images/bucket-name/background-processed.png'
+        }
+      ]
     }
-  })
-
-  it('type text', () => {
-    data.watermarkOptions = {
-      type: 'text',
-      details: {
-        // eslint-disable-next-line quotes
-        text: "Artes UP\n91 98888-8888",
-        size: 15,
-        color: '#AAF122'
-      }
-    }
-
-    assert.strictEqual(true, validator.validate(data, schema))
   })
 
   it('type image', () => {
-    data.watermarkOptions = {
-      type: 'image',
-      details: {
-        path: 'images/bucket-name/test.png'
-      }
-    }
-
     assert.strictEqual(true, validator.validate(data, schema))
+    assert.deepStrictEqual([], validator.getErrors())
   })
 })
