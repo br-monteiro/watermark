171edc764be74e32be8346d609d9a307d9ad9b57
Edinho <bruno.monteirodg@gmail.com>
2020-09-20 01:23:01 -0300

    refactor: changed fetchFile function and added fileExistsInS3

--- src/aws/bucket-manager.js
+++ src/aws/bucket-manager.js
@@ -5,34 +5,54 @@ const s3 = require('./s3')
 /**
  * Save the file from S3 bucket in local file system
  * @param { string } bucket - The bucket name
- * @param { string } filename - The file name
+ * @param { string } filename - The file name on bucket
+ * @param { string } destination - The file name to be saved
  * @return { Promise<Boolean> }
  */
-function fetchFile (bucket, filename) {
-  return new Promise((resolve) => {
-    try {
-      const fileStream = fileManager.writeStream(filename)
-      var s3Stream = s3
-        .getObject({ Bucket: bucket, Key: filename })
-        .createReadStream()
+async function fetchFile (bucket, filename, destination) {
+  try {
+    if (!await fileExistsInS3(bucket, filename)) {
+      return false
+    }
 
-      s3Stream.on('error', err => {
-        throw new Error(err)
+    const fileStream = fileManager.writeStream(destination)
+    const s3Stream = s3
+      .getObject({ Bucket: bucket, Key: filename })
+      .createReadStream()
+
+    s3Stream
+      .on('error', err => {
+        log.error(err, 'error occur while fetch file')
       })
 
-      s3Stream
-        .pipe(fileStream)
-        .on('error', err => {
-          throw new Error(err)
-        })
-        .on('close', () => {
-          resolve(true)
-        })
-    } catch (error) {
-      log.error(error, 'error occur while fetch file')
-      resolve(false)
-    }
-  })
+    s3Stream
+      .pipe(fileStream)
+      .on('error', err => {
+        log.error(err, 'error occur while fetch file')
+      })
+      .on('close', async () => {
+        Promise.resolve(true)
+      })
+  } catch (error) {
+    log.error(error, 'error occur while fetch file')
+    return false
+  }
+}
+
+/**
+ * Check if the file exists in S3
+ * @param { string } bucket - The bucket name
+ * @param { string } filename - The file name on bucket
+ * @return { Promise<Boolean> }
+ */
+async function fileExistsInS3 (bucket, filename) {
+  try {
+    await s3.headObject({ Bucket: bucket, Key: filename }).promise()
+    return true
+  } catch (error) {
+    log.error(error, `there's no file in S3: ${filename}`)
+    return false
+  }
 }
 
 /**
