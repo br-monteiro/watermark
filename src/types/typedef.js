054a4091075b4d754fa9fe16cae2ad493e68636f
Edinho <bruno.monteirodg@gmail.com>
2020-09-20 01:21:00 -0300

    docs: added new typedefs

--- src/types/typedef.js
+++ src/types/typedef.js
@@ -47,3 +47,51 @@
  * @property { String } message
  * @property { String } path
  */
+
+/**
+ * @typedef QueueDatabaseRow
+ * @property { Number } id
+ * @property { 'queued' | 'processing' | 'processed' | 'error' } status
+ * @property { String } transaction_id
+ * @property { String } feedback_url
+ * @property { String } watermark_path
+ */
+
+/**
+ * @typedef QueueItemsDatabaseRow
+ * @property { Number } id
+ * @property { Number } queue_id
+ * @property { 'queued' | 'processing' | 'processed' | 'error' } status
+ * @property { Number } position_x
+ * @property { Number } position_y
+ * @property { Number } position_height
+ * @property { Number } position_width
+ * @property { String } base_image_path
+ * @property { String } s3_image_path
+ * @property { String } details
+ */
+
+/**
+ * @typedef FileDetails
+ * @property { String } bucket
+ * @property { String } fullPath
+ * @property { String } fileName
+ * @property { Boolean } downloaded
+ * @property { Boolean } uploaded
+ * @property { String } details
+ */
+
+/**
+ * @typedef SynchronizedItem
+ * @property { String } transactionId
+ * @property { String } feedbackUrl
+ * @property { FileDetails } watermark
+ * @property { Number } x
+ * @property { Number } y
+ * @property { Number } height
+ * @property { Number } width
+ * @property { FileDetails } baseImage
+ * @property { FileDetails } s3Image
+ * @property { 'success' | 'error' } status
+ * @property { Array<String> } details
+ */
