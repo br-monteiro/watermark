1e532f193c4760d773f44d7308f80c6694aede22
Edinho <bruno.monteirodg@gmail.com>
2020-09-20 22:00:53 -0300

    test: added tests for pathname

--- src/file-manager/index.test.js
+++ src/file-manager/index.test.js
@@ -1,9 +1,10 @@
 const assert = require('assert')
-const { fileExists, basename } = require('./')
+const { fileExists, basename, pathname } = require('./')
+const { staticPaths } = require('../config')
 
 describe('file-manage', () => {
   it('fileExists', () => {
-    assert.strictEqual(fileExists('.gitkeep'), true)
+    assert.strictEqual(fileExists(`${staticPaths.input}.gitkeep`), true)
     assert.strictEqual(fileExists('testeteteste.jpg'), false)
     assert.strictEqual(fileExists(''), false)
     assert.strictEqual(fileExists(), false)
@@ -15,5 +16,16 @@ describe('file-manage', () => {
     assert.strictEqual(basename('test'), 'test')
     assert.strictEqual(basename(''), undefined)
     assert.strictEqual(basename(), undefined)
+  })
+
+  it('pathname', () => {
+    assert.strictEqual(pathname('images/teste/test.jpg'), 'images/teste/')
+    assert.strictEqual(pathname('images/teste/'), 'images/teste/')
+    assert.strictEqual(pathname('images/teste'), 'images/')
+    assert.strictEqual(pathname('images/'), 'images/')
+    assert.strictEqual(pathname(''), '')
+    assert.strictEqual(pathname(), undefined)
+    assert.strictEqual(pathname(false), undefined)
+    assert.strictEqual(pathname([]), undefined)
   })
 })
