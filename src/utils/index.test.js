b728139d763597ec3099373657f37af077656141
Edinho <bruno.monteirodg@gmail.com>
2020-09-20 22:04:52 -0300

    test: added tests for getUrlDetails

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -14,4 +14,24 @@ describe('Utils functions', () => {
     assert.deepStrictEqual(utils.safeArray([1, 2, 3]), [1, 2, 3])
     assert.deepStrictEqual(utils.safeArray([{ id: true }, { id: false }]), [{ id: true }, { id: false }])
   })
+
+  it('getUrlDetails', () => {
+    assert.deepStrictEqual(utils.getUrlDetails(), undefined)
+    assert.deepStrictEqual(utils.getUrlDetails(''), undefined)
+    assert.deepStrictEqual(utils.getUrlDetails(false), undefined)
+    assert.deepStrictEqual(utils.getUrlDetails('http://test.com'), {
+      hostname: 'test.com',
+      protocol: 'http:',
+      port: 80,
+      pathname: '/',
+      search: ''
+    })
+    assert.deepStrictEqual(utils.getUrlDetails('https://test.com:8000/test-route?test=true'), {
+      hostname: 'test.com',
+      protocol: 'https:',
+      port: 8000,
+      pathname: '/test-route',
+      search: '?test=true'
+    })
+  })
 })
