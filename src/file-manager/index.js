3c1ccbf0d50338fc50710d005fb8dcd95d2273eb
Edinho <bruno.monteirodg@gmail.com>
2020-09-20 22:03:07 -0300

    feat: added functions basename, pathname and buildThumbnailPath

--- src/file-manager/index.js
+++ src/file-manager/index.js
@@ -13,7 +13,7 @@ function fileExists (filename) {
   }
 
   try {
-    return fs.existsSync(`${staticPaths.input}${filename}`)
+    return fs.existsSync(filename)
   } catch (_) {
     return false
   }
@@ -30,7 +30,7 @@ function writeStream (filename) {
     throw new Error('The `path` must to be a valid string')
   }
 
-  return fs.createWriteStream(`${staticPaths.output}${filename}`)
+  return fs.createWriteStream(filename)
 }
 
 /**
@@ -44,7 +44,7 @@ function readStream (filename) {
     throw new Error('The `path` must to be a valid string')
   }
 
-  return fs.createReadStream(`${staticPaths.output}${filename}`)
+  return fs.createReadStream(filename)
 }
 
 /**
@@ -56,13 +56,63 @@ function readStream (filename) {
  */
 function basename (filename) {
   return filename && typeof filename === 'string'
-    ? path.basename(`${staticPaths.output}${filename}`)
+    ? path.basename(filename)
     : undefined
+}
+
+/**
+ * Returns the path without file name
+ * @param { string } filename - The file name
+ * @return { string }
+ */
+function pathname (filename) {
+  if (typeof filename !== 'string') {
+    return
+  }
+
+  const chunks = filename.split('/')
+
+  if (chunks.length === 1) {
+    return filename
+  }
+
+  delete chunks[chunks.length - 1]
+
+  return chunks.join('/')
+}
+
+/**
+ * Create one directory in static/input and static/output
+ * @param { string } directoryName - The name of directory
+ * @return { boolean }
+ */
+async function createDirInStatic (directoryName) {
+  try {
+    fs.mkdirSync(`${staticPaths.input}${directoryName}`, { recursive: true })
+    fs.mkdirSync(`${staticPaths.output}${directoryName}`, { recursive: true })
+    return true
+  } catch (_) {
+    return false
+  }
+}
+
+/**
+ * Returns the filename with prefix
+ * @param { string } filename - The full path of file
+ * @param { string } prefix - The prefix of file
+ * @return { string }
+ */
+function buildThumbnailPath (filename, prefix = 'thumb-') {
+  const prefixFileName = `${prefix}${basename(filename)}`
+  return filename.replace(new RegExp(basename(filename) + '$'), prefixFileName)
 }
 
 module.exports = {
   fileExists,
   writeStream,
   readStream,
-  basename
+  basename,
+  pathname,
+  buildThumbnailPath,
+  createDirInStatic
 }
