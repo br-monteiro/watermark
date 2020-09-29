ac97b90d3fd4908f8be1817b6e6b3b2c56bba887
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-29 20:36:54 -0300

    Add DB_PORT env

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ config.knex = {
     client: 'mysql',
     connection: {
       host: process.env.DB_HOST,
-      port: '3306',
+      port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE
