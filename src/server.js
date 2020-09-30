1ab810634a265700a26a6943f08cfd408fe9d103
Edinho <bruno.monteirodg@gmail.com>
2020-09-29 23:41:49 -0300

    refactor: using function checkDatabaseConnection

--- src/server.js
+++ src/server.js
@@ -2,17 +2,20 @@
 const app = require('./index')
 const config = require('./config')
 const bole = require('bole')
+const { checkDatabaseConnection } = require('./application-status/databases-status')
 
 bole.output({ level: 'debug', stream: process.stdout })
 const log = bole('server')
 
 log.info('server process starting')
 
-app.listen(config.express.port, function (error) {
+app.listen(config.express.port, async error => {
   if (error) {
     log.error('unable to listen for connections', error)
     process.exit(10)
   }
 
   log.info(`express is listening on port ${config.express.port}`)
+
+  await checkDatabaseConnection()
 })
