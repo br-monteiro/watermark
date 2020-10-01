29afb4b928c448760959eda7d2ac34ad5a7f2131
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-10-01 13:48:35 -0300

    Use DB_PORT env in production

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ config.knex = {
     client: 'mysql',
     connection: {
       host: process.env.DB_HOST,
-      port: '3306',
+      port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_DATABASE,
       database: process.env.DB_PASSWORD
