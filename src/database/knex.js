a74aa64f598d99458b1377479757a7a66180ec84
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-10-01 21:08:02 -0300

    Debug environment

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,6 +1,9 @@
 const config = require('../config')
 const env = config.env.PRD ? 'production' : 'development'
 
+console.log('environment db', env)
+console.log(config.knex)
+
 const knex = require('knex')(config.knex[env])
 
 module.exports = knex
